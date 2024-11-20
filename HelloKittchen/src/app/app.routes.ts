e3fb943781857338145f6af2f745c46f2b22a931
uliaray <raysantiagodel20@gmail.com>
2024-11-20 10:28:38 -0300

    pt3

--- HelloKittchen/src/app/app.routes.ts
+++ HelloKittchen/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './layout/pages/home/home.component';
+import { NewsComponent } from './layout/pages/news/news.component';
+import { CooksComponent } from './layout/pages/cooks/cooks.component';
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent}
-
+  {path: '', component: HomeComponent},
+  {path: 'news', component: NewsComponent},
+  {path: 'cooks', component: CooksComponent}
 ];
